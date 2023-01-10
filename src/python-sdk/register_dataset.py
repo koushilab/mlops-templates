1bb4d2ee40df9f3bad552b09dc63bdf962b154c0
kkadambi <53005786+koushilab@users.noreply.github.com>
2023-01-10 18:43:49 +0530

    Update register_dataset.py

--- src/python-sdk/register_dataset.py
+++ src/python-sdk/register_dataset.py
@@ -9,8 +9,8 @@ def parse_args():
     parser.add_argument("-n", type=str, help="Name of the dataset you want to register")
     parser.add_argument("-d", type=str, help="Description of the dataset you want to register")
     parser.add_argument("-t", type=str, help="type of dataset", default='local')    
-    parser.add_argument("-l", type=str, help="local path of the dataset folder", default='classical/python-sdk/data/')
-    parser.add_argument("-p", type=str, help="Path on data store", default='classical/python-sdk/data/')
+    parser.add_argument("-l", type=str, help="local path of the dataset folder", default='data/training/')
+    parser.add_argument("-p", type=str, help="Path on data store", default='dogs-labels/')
     parser.add_argument("-s", type=str, help="Storage url for cloud storage")
     return parser.parse_args()
 
@@ -20,9 +20,15 @@ def main():
     ws = Workspace.from_config()
     if args.t == "local":
         print("local data")
+        print(f'Name of the dataset {args.n}')
+        print(f'Description of the dataset {args.d}')
+        print(f'Type of the dataset {args.t}')
+        print(f'Localpath of the dataset {args.l}')
+        print(f'DataStorePath of the dataset {args.p}')
+        print(f'Storageurl of the dataset {args.s}')
         datastore = ws.get_default_datastore()
         print("Testing by Koushi")
-        Dataset.File.upload_directory(src_dir = 'classical/python-sdk/data/training/', target = (datastore, args.p), overwrite = True, show_progress = True)
+        Dataset.File.upload_directory(src_dir = args.l, target = (datastore, args.p), overwrite = True, show_progress = True)
         print(f"About to register dataset {args.n}")
 
         dataset = Dataset.File.from_files(path=[(datastore, args.p)], validate=True)
